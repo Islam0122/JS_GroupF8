52cbbe389286c103a5c9bc99c0f5de8b4f4247aa
islam <dujsobaevislam01@gmail.com>
2024-11-11 19:25:50 +0600

    lesson->3  >'test'<

--- lesson_3/Js.js
+++ lesson_3/Js.js
@@ -17,3 +17,30 @@
 //             result=>console.log("Fulfilled: " + result),
 //             error=>console.log("Rejected: " + error.message)
 //         )
+
+
+
+const img_url = './img.png'
+// const imgEl = document.createElement('img')
+const h1El = document.createElement('h1')
+const imgEl = document.querySelector('img')
+
+let code = 201
+const p = new Promise((success,error)=>{
+    setTimeout(()=> {
+        if (code===200){
+            success(img_url)
+        }
+        else{
+            error('404 no found img url  ')
+        }
+    },2000)
+})
+
+p
+    .then(
+    url => imgEl.src = url,
+)
+// .catch(error => console.log(error))
+    .catch(error => document.body.append(h1El.innerText=error))
+    .finally(()=> document.body.append(document.createElement('h2').innerText="hello my boss"))
