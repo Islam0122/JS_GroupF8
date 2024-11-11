2981a367ccc99d6c733398f47086e4238dd76865
islam <dujsobaevislam01@gmail.com>
2024-11-11 18:54:19 +0600

    lesson->3  >'promise'<

---
+++ lesson_3/Js.js
@@ -0,0 +1,19 @@
+// const promise = new Promise((resolve, reject) => {
+//
+//     setTimeout(() => {
+//         resolve('text');
+//         reject('error');
+//     }, 2000);
+//
+// });
+// promise
+//     .then( data => {
+//     console.log(data);
+//      })
+//     .catch((err)=>{console.log(err)})
+//
+// promise
+//         .then(
+//             result=>console.log("Fulfilled: " + result),
+//             error=>console.log("Rejected: " + error.message)
+//         )
